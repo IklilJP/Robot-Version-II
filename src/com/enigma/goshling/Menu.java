2355b57b7da446600f3577c844fcd1e2c57f8724
ikliljayaperwira85 <ikliljayaperwira85@gmail.com>
2024-06-05 22:31:31 +0700

    Robot version II validasi

--- src/com/enigma/goshling/Menu.java
+++ src/com/enigma/goshling/Menu.java
@@ -4,34 +4,32 @@ public class Menu {
 
     private Robot robot;
     private MataAingin mataAngin;
+    private String[] arrayInput;
+    private String command;
 
     public void menu() {
         System.out.println("===========================================");
         System.out.println("Selamat Datang di Robot versi II");
         System.out.println("===========================================");
-        System.out.print("Tentukan posisi awal robot: ");
-        String posisiAwal = Helper.inputString();
 
-        String[] arrayInput = posisiAwal.split(",");
-
-        if (arrayInput.length != 3 || arrayInput[0].length() != 1 || !Helper.checkCommand(arrayInput[0]) || !Helper.checkInteger(arrayInput[1]) || !Helper.checkInteger(arrayInput[2])) {
-            menu();
-        }
+        do {
+            System.out.print("Tentukan posisi awal robot: ");
+            String posisiAwal = Helper.inputString();
+            arrayInput = posisiAwal.split(",");
+        } while (arrayInput.length != 3 || arrayInput[0].length() != 1 || !Helper.checkCommand(arrayInput[0]) || !Helper.checkInteger(arrayInput[1]) || !Helper.checkInteger(arrayInput[2]));
 
         robot = new Robot(arrayInput[0], Helper.stringToInt(arrayInput[1]), Helper.stringToInt(arrayInput[2]));
 
-        System.out.println("Input perintah : ");
+        do {
+            System.out.println("Input perintah : ");
 
-        String command = Helper.inputString();
+            command = Helper.inputString();
+        } while (!command.matches("[a-zA-Z]+") || !command.matches("[AaLlRr\\s]*"));
 
-        if (!command.matches("[a-zA-Z]+") || !command.matches("[AaLlRr\\s]*")) {
-            menu();
-        }
         String[] perintah = command.toUpperCase().split("");
         robot.directtion(perintah);
         System.out.println("Posisi Akhir Robot: " + robot.getSumbuX() + "," + robot.getSumbuY() + "," + robot.getPosisi());
 
     }
-
 
 }
